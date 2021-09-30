d48a83303169d398dc86a2d3b8f3addf9ac6cafc
Rifqy Anugerah <rifqyanugerah@student.telkomuniversity.ac.id>
2021-09-30 14:32:23 +0700

    TugasImplementasiAlgoritma.java

---
+++ Praktikum 2/TugasImplementasiAlgoritma.java
@@ -0,0 +1,10 @@
+public class TugasImplementasiAlgoritma {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama : Rifqy Anugerah");
+		System.out.println("Kelas: D3 RPLA 45-05");
+		System.out.println("NIM  : 6706213095");
+		System.out.println("IPK  : 4.0");
+	}
+}
+	
